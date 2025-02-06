f63662d0041e1a04411aa5e9231aef78e7afd4d5
kacygd <164707129+kacygd@users.noreply.github.com>
2025-02-06 22:13:15 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,43 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/LevelInfoLayer.hpp>
+#include <Geode/modify/MenuLayer.hpp>
 #include <Geode/utils/web.hpp>
 #include <Geode/loader/Loader.hpp>
 
 using namespace geode::prelude;
 
 std::unordered_map<int, int> cachedPositions;
+bool requestFinished = false;
 bool initialized = false;
 
 EventListener<web::WebTask> webRequest;
 
 void createButton(CCLayer* self, CCLabelBMFont* label, int levelID) {
     CCPoint position = { label->getPositionX() + 8.f, label->getPositionY() };
-    auto button = CCMenuItemSpriteExtra::create(label, self, menu_selector([](CCObject* ret) {
-        int levelID = static_cast<CCInteger*>(static_cast<CCNode*>(ret)->getUserObject())->getValue();
-        std::string url = "https://cps.ps.fhgdps.com/database/data/levels/" + std::to_string(levelID);
-        web::openLinkInBrowser(url.c_str());
-    }));
+    auto button = CCMenuItemSpriteExtra::create(label, self, nullptr); // No action required here
     auto menu = CCMenu::create();
-    menu->setPosition(position);
+    auto trophy = CCSprite::createWithSpriteFrameName("rankIcon_top50_001.png");
+    trophy->setScale(0.5f);
+    trophy->setPosition({ -10.f, 5.f });
     menu->addChild(button);
+    menu->setPosition(position);
+    button->addChild(trophy);
     self->addChild(menu);
 }
 
+void infoButton(CCLayer* layer, CCLabelBMFont* label, bool internetFail = false) {
+    CCPoint position = { label->getPositionX() - 122, label->getPositionY() - 81 };
+    auto buttonBg = CCSprite::createWithSpriteFrameName("GJ_infoIcon_001.png");
+    auto button = CCMenuItemSpriteExtra::create(buttonBg, layer, nullptr); // No action required here
+    auto menu = CCMenu::create();
+    menu->setScale(0.5f);
+    menu->addChild(button);
+    menu->setPosition(position);
+    layer->addChild(menu);
+}
+
 void getRequest(CCLayer* self, GJGameLevel* level, CCLabelBMFont* label) {
-    self->retain();  // Giữ lại self trong khi yêu cầu web chưa hoàn thành
+    self->retain();
     int levelID = level->m_levelID;
     std::string url = "https://cps.ps.fhgdps.com/database/demonlist.php?levelID=" + std::to_string(levelID);
 
@@ -32,17 +45,17 @@ void getRequest(CCLayer* self, GJGameLevel* level, CCLabelBMFont* label) {
         if (web::WebResponse* res = e->getValue()) {
             std::string result = res->string().unwrap();
             int position = std::stoi(result);
-
             if (position > 0) {
                 label->setString(std::to_string(position).c_str());
+                auto pos = CCInteger::create(levelID);
                 createButton(self, label, levelID);
                 cachedPositions.insert({ levelID, position });
             } else {
-                label->setString("N/A");
+                label->setString("N/A"); // Directly show "N/A" when no rank is available
                 cachedPositions.insert({ levelID, -1 });
             }
         } else if (e->isCancelled()) {
-            label->setString("???");
+            label->setString("???"); // Show "???" if the request fails
         }
         self->autorelease();
     });
@@ -55,7 +68,7 @@ class $modify(LevelInfoLayer) {
     bool init(GJGameLevel* level, bool idk) {
         if (!LevelInfoLayer::init(level, idk)) return false;
 
-        if (level->m_demon != 1) return true;  // Nếu không phải level demon, bỏ qua
+        if (level->m_demon != 1) return true;
 
         int offset = (level->m_coins == 0) ? 17 : 4;
         auto director = CCDirector::sharedDirector();
@@ -72,7 +85,7 @@ class $modify(LevelInfoLayer) {
                 label->setString(std::to_string(position).c_str());
                 createButton(this, label, level->m_levelID);
             } else {
-                label->setString("N/A");
+                label->setString("N/A"); // Directly show "N/A"
             }
         } else {
             getRequest(this, level, label);
