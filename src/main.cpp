fa7b2539026e0f36c151d6c988275e2b3cf0f195
kacygd <164707129+kacygd@users.noreply.github.com>
2025-02-07 13:38:35 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,32 +7,26 @@
 using namespace geode::prelude;
 
 std::unordered_map<int, int> cachedPositions;
-bool requestFinished = false;
 bool initialized = false;
 
 EventListener<web::WebTask> webRequest;
 
-void onButtonPressed(CCObject* sender) {
-    // Hàm callback khi bấm vào nút
-    CCLog("Button pressed!");
+std::string getRankTexture(int rank) {
+    if (rank == 1) return "rankIcon_1_001.png";
+    if (rank <= 10) return "rankIcon_top10_001.png";
+    if (rank <= 20) return "rankIcon_top50_001.png";
+    if (rank <= 35) return "rankIcon_top100_001.png";
+    if (rank <= 50) return "rankIcon_top200_001.png";
+    if (rank <= 75) return "rankIcon_top500_001.png";
+    if (rank <= 100) return "rankIcon_top1000_001.png";
+    return "rankIcon_all_001.png";
 }
 
 void createButton(CCLayer* self, CCLabelBMFont* label, int levelID, int rank) {
     CCPoint position = { label->getPositionX() + 8.f, label->getPositionY() };
-
-    std::string texture;
-    if (rank == 1) texture = "rankIcon_1_001.png";
-    else if (rank <= 10) texture = "rankIcon_top10_001.png";
-    else if (rank <= 20) texture = "rankIcon_top50_001.png";
-    else if (rank <= 35) texture = "rankIcon_top100_001.png";
-    else if (rank <= 50) texture = "rankIcon_top200_001.png";
-    else if (rank <= 75) texture = "rankIcon_top500_001.png";
-    else if (rank <= 100) texture = "rankIcon_top1000_001.png";
-    else texture = "rankIcon_all_001.png";
-
-    auto button = CCMenuItemSpriteExtra::create(label, self, menu_selector(onButtonPressed));
+    auto button = CCMenuItemSpriteExtra::create(label, self, nullptr);
     auto menu = CCMenu::create();
-    auto trophy = CCSprite::createWithSpriteFrameName(texture.c_str());
+    auto trophy = CCSprite::createWithSpriteFrameName(getRankTexture(rank).c_str());
     trophy->setScale(0.5f);
     trophy->setPosition({ -10.f, 5.f });
     menu->addChild(button);
@@ -41,8 +35,8 @@ void createButton(CCLayer* self, CCLabelBMFont* label, int levelID, int rank) {
     self->addChild(menu);
 }
 
+
 void getRequest(CCLayer* self, GJGameLevel* level, CCLabelBMFont* label) {
-    self->retain();
     int levelID = level->m_levelID;
     std::string url = "https://cps.ps.fhgdps.com/database/demonlist.php?levelID=" + std::to_string(levelID);
 
@@ -51,21 +45,16 @@ void getRequest(CCLayer* self, GJGameLevel* level, CCLabelBMFont* label) {
             std::string result = res->string().unwrap();
             try {
                 int position = std::stoi(result);
-                if (position > 0) {
-                    label->setString(std::to_string(position).c_str());
-                    cachedPositions[levelID] = position;
-                    createButton(self, label, levelID, position);
-                } else {
-                    label->setString("N/A");
-                    cachedPositions[levelID] = -1;
-                }
-            } catch (const std::exception&) {
-                label->setString("???");
+                cachedPositions[levelID] = (position > 0) ? position : -1;
+                label->setString((position > 0) ? std::to_string(position).c_str() : "N/A");
+                if (position > 0) createButton(self, label, levelID);
+            } catch (...) {
+                label->setString("N/A");
+                cachedPositions[levelID] = -1;
             }
         } else {
             label->setString("???");
         }
-        self->release();
     });
 
     auto req = web::WebRequest();
@@ -87,12 +76,8 @@ class $modify(LevelInfoLayer) {
         label->setScale(0.5f);
 
         if (it != cachedPositions.end()) {
-            if (it->second > -1) {
-                label->setString(std::to_string(it->second).c_str());
-                createButton(this, label, level->m_levelID, it->second);
-            } else {
-                label->setString("N/A");
-            }
+            label->setString((it->second > -1) ? std::to_string(it->second).c_str() : "N/A");
+            if (it->second > -1) createButton(this, label, level->m_levelID);
         } else {
             getRequest(this, level, label);
         }
