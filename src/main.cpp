cdb23c4f9ecc1e2f6031c1c28ac210fda4a8bcd6
kacygd <164707129+kacygd@users.noreply.github.com>
2025-02-07 13:48:15 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,22 +11,11 @@ bool initialized = false;
 
 EventListener<web::WebTask> webRequest;
 
-std::string getRankTexture(int rank) {
-    if (rank == 1) return "rankIcon_1_001.png";
-    if (rank <= 10) return "rankIcon_top10_001.png";
-    if (rank <= 20) return "rankIcon_top50_001.png";
-    if (rank <= 35) return "rankIcon_top100_001.png";
-    if (rank <= 50) return "rankIcon_top200_001.png";
-    if (rank <= 75) return "rankIcon_top500_001.png";
-    if (rank <= 100) return "rankIcon_top1000_001.png";
-    return "rankIcon_all_001.png";
-}
-
-void createButton(CCLayer* self, CCLabelBMFont* label, int levelID, int rank) {
+void createButton(CCLayer* self, CCLabelBMFont* label, int levelID) {
     CCPoint position = { label->getPositionX() + 8.f, label->getPositionY() };
     auto button = CCMenuItemSpriteExtra::create(label, self, nullptr);
     auto menu = CCMenu::create();
-    auto trophy = CCSprite::createWithSpriteFrameName(getRankTexture(rank).c_str());
+    auto trophy = CCSprite::createWithSpriteFrameName("rankIcon_top10_001.png");
     trophy->setScale(0.5f);
     trophy->setPosition({ -10.f, 5.f });
     menu->addChild(button);
@@ -35,9 +24,7 @@ void createButton(CCLayer* self, CCLabelBMFont* label, int levelID, int rank) {
     self->addChild(menu);
 }
 
-
 void getRequest(CCLayer* self, GJGameLevel* level, CCLabelBMFont* label) {
-    self->retain();
     int levelID = level->m_levelID;
     std::string url = "https://cps.ps.fhgdps.com/database/demonlist.php?levelID=" + std::to_string(levelID);
 
@@ -46,21 +33,16 @@ void getRequest(CCLayer* self, GJGameLevel* level, CCLabelBMFont* label) {
             std::string result = res->string().unwrap();
             try {
                 int position = std::stoi(result);
-                if (position > 0) {
-                    label->setString(std::to_string(position).c_str());
-                    createButton(self, label, levelID, position);
-                    cachedPositions.insert({ levelID, position });
-                } else {
-                    label->setString("N/A");
-                    cachedPositions.insert({ levelID, -1 });
-                }
+                cachedPositions[levelID] = (position > 0) ? position : -1;
+                label->setString((position > 0) ? std::to_string(position).c_str() : "N/A");
+                if (position > 0) createButton(self, label, levelID);
             } catch (...) {
-                label->setString("???");
+                label->setString("N/A");
+                cachedPositions[levelID] = -1;
             }
         } else {
             label->setString("???");
         }
-        self->autorelease();
     });
 
     auto req = web::WebRequest();
@@ -83,7 +65,7 @@ class $modify(LevelInfoLayer) {
 
         if (it != cachedPositions.end()) {
             label->setString((it->second > -1) ? std::to_string(it->second).c_str() : "N/A");
-            if (it->second > -1) createButton(this, label, level->m_levelID, it->second);
+            if (it->second > -1) createButton(this, label, level->m_levelID);
         } else {
             getRequest(this, level, label);
         }
